148415a8fa10233ebb2131138a5bf51478b1404a
Mohmn <naqashmohmin1@gmail.com>
2021-03-06 21:34:04 +0530

    urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('blogs/', views.blog_list),
-    # path('blogs/<int:pk>/', views.blog_detail),
     path('blogs/', views.ListBlogs.as_view()),
-    path('blogs/<int:pk>/',views.BlogDetail.as_view())
+    path('blogs/<int:pk>/',views.BlogDetail.as_view()),
+    path('register/',views.createUser.as_view()),
+    path('user/<int:pk>/',views.updateUsers.as_view()),
 ]
